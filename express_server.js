71f245c8b09dd69a4795677b60efe48eb676e8d1
Ana <anmeuri@gmail.com>
2023-10-30 19:59:38 -0700

    Add second route and template for single URL page

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { render } = require("ejs");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -16,6 +17,11 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
